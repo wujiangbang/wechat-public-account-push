0f92ee190df8ecd9a9d982bf7790fa90ebc3dc47
Wu jiangbang <42349644+wujiangbang@users.noreply.github.com>
2023-12-20 19:25:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -2,66 +2,81 @@
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  USE_PASSAGE: 'push-deer',
 
-  // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  // 使用微信测试号时才需要填写：公众号APP_ID
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  // APP_ID: 'wx75ad7fdc90494fcc',
 
-  // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  // 使用微信测试号时才需要填写：公众号APP_SECRET
+  // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
+  // 如果你非要填这里也行。脚本也能运行
+  // APP_SECRET: '8e7019d171d47627e2fc60993561de3e',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '青浦区',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      // id:'PDU25371Tr6LMXm5jsKyZzs4zRVzasOlTBbYWeptk',
+      id: 'PDU25371TzW1vWaCRsvQPE3S8AmVPdXZd55g7mBgl',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-09',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '本周',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: 'https://weibo.com/',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日',
+          name: '宝贝',
+          year: '1996',
+          date: '08-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日',
+          name: '李四',
+          year: '1996',
+          date: '10-09',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日',
+          name: '相识纪念日',
+          year: '2023',
+          date: '12-20',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2023-09-08' },
       ],
     },
   ],
 
-
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
+      // id:'PDU25371Tr6LMXm5jsKyZzs4zRVzasOlTBbYWeptk',
+      id: 'PDU25371TzW1vWaCRsvQPE3S8AmVPdXZd55g7mBgl',
+    },
   ],
+};
 
-}
-
-module.exports = USER_CONFIG
-
+module.exports = USER_CONFIG;
